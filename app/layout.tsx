1a6d758e54bc2178863517fa441d205bb18c45d3
Badini Ibrahim <sawadogo.badiniibrahim@gmail.com>
2024-08-06 15:24:22 +0200

    chore:update readme.md

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,14 +2,12 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
-import { getServerUrl } from "@/get-server-url";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Book Shelf",
   description: "The best reading or listening experience on any computer",
-  metadataBase: new URL(getServerUrl()),
 };
 
 export default function RootLayout({
