62f1ff3c9f2d594455f2d3835a81243b5ff6d82c
Badini Ibrahim <sawadogo.badiniibrahim@gmail.com>
2024-08-06 15:29:16 +0200

    chore:update readme.md

--- src/infrastructure/api/apiClient.ts
+++ src/infrastructure/api/apiClient.ts
@@ -1,9 +1,6 @@
-import { validateConfig } from "@/src/lib/validation";
 import axios from "axios";
 
 const baseURL = process.env.NEXT_PUBLIC_API_URI!;
-
-validateConfig(baseURL);
 
 const apiClient = axios.create({
   baseURL,
